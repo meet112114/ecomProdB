de41c7fb1d00bcde506c475b68c2cf30968f1aaf
meet112114 <118681492+meet112114@users.noreply.github.com>
2024-10-16 22:28:14 +0530

    Update passport.js

--- router/passport.js
+++ router/passport.js
@@ -14,7 +14,7 @@ passport.deserializeUser((user, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID ,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL ,
+    callbackURL: "https://ecomprodb.onrender.com/auth/google/callback" ,
     scope: ['profile' , 'email']
 },
 function(accessToken, refreshToken, profile, cb) {
